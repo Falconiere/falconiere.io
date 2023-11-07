5fbc0466491e65fe1a990ef4b1cb4fc0ba8323ef
Falconiere R. Barbosa <falconieer@gmail.com>
2023-11-07 19:35:16 -0300

    trying new folder directory

--- src/server-actions/email.ts
+++ src/server-actions/email.ts
@@ -6,8 +6,8 @@ import { readFileSync } from 'fs';
 
 const getHtmlPath = (dir: string) => join(process.cwd(), dir);
 const submitWelcomeEmailForNewsLetterSignup = async (email: string) => {
-  await saveEmailFromNewsLetterSignup(email);
   const html = readFileSync(getHtmlPath('public/email/templates/welcome.html'), 'utf8');
+  await saveEmailFromNewsLetterSignup(email);
   const resend = new Resend(process.env.RESEND_API_KEY);
   await resend.emails.send({
     from: 'hello@falconiere.io',
