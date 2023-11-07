b3f4766480c45ae895a3b3c4d32f4de9aaa68931
Falconiere R. Barbosa <falconieer@gmail.com>
2023-11-07 19:00:28 -0300

    check error

--- src/server-actions/email.ts
+++ src/server-actions/email.ts
@@ -1,13 +1,12 @@
 "use server"
 import { Resend } from 'resend';
 import { saveEmailFromNewsLetterSignup } from './supabase';
-import { join } from 'path';
 import { readFileSync } from 'fs';
 
-const getHtmlPath = (dir: string) => join(process.cwd(), dir);
+
 const submitWelcomeEmailForNewsLetterSignup = async (email: string) => {
+  const html = readFileSync('src/email/templates/welcome.html', 'utf8');
   await saveEmailFromNewsLetterSignup(email);
-  const html = readFileSync(getHtmlPath('src/email/templates/welcome.html'), 'utf8');
   const resend = new Resend(process.env.RESEND_API_KEY);
   await resend.emails.send({
     from: 'hello@falconiere.io',
