aae7ffd15f453f62bec33ae74795ba7b181dc31c
Falconiere R. Barbosa <falconieer@gmail.com>
2023-11-07 14:10:14 -0300

    update mobile

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
 
 const Page = () => (
   <div className="flex items-center justify-center min-h-[60vh]">
-    <h2 className="animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-pink-400 bg-clip-text text-transparent text-3xl sm:text-4xl font-bold">
+    <h2 className="animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-pink-400 bg-clip-text text-transparent text-2xl sm:text-4xl font-bold">
       Projects coming soon...
     </h2>
   </div>
