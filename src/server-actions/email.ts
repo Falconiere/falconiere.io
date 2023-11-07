94704a463022d5007e0d60ecf6ce7926d54b6dc4
Falconiere R. Barbosa <falconieer@gmail.com>
2023-11-07 19:57:41 -0300

    this is annoying 2

--- src/server-actions/email.ts
+++ src/server-actions/email.ts
@@ -4,7 +4,7 @@ import { saveEmailFromNewsLetterSignup } from './supabase';
 import { promises as fs } from 'fs';
 
 const submitWelcomeEmailForNewsLetterSignup = async (email: string) => {
-  const html = await fs.readFile(process.cwd() + 'src/app/email/templates/welcome.html', 'utf8');
+  const html = await fs.readFile(process.cwd() + '/src/app/email/templates/welcome.html', 'utf8');
   await saveEmailFromNewsLetterSignup(email);
   const resend = new Resend(process.env.RESEND_API_KEY);
   await resend.emails.send({
