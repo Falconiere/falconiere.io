dd6b4e2fe08b210a54f3b6efe3ee677663c40531
Falconiere R. Barbosa <falconieer@gmail.com>
2023-11-07 20:02:33 -0300

    I gave up

--- src/server-actions/email.ts
+++ src/server-actions/email.ts
@@ -1,17 +1,15 @@
 "use server"
 import { Resend } from 'resend';
 import { saveEmailFromNewsLetterSignup } from './supabase';
-import { promises as fs } from 'fs';
-
+import welcome from '@/templates/welcome';
 const submitWelcomeEmailForNewsLetterSignup = async (email: string) => {
-  const html = await fs.readFile(process.cwd() + '/src/app/email/templates/welcome.html', 'utf8');
   await saveEmailFromNewsLetterSignup(email);
   const resend = new Resend(process.env.RESEND_API_KEY);
   await resend.emails.send({
     from: 'hello@falconiere.io',
     to: email,
     subject: 'NewsLetter Signup',
-    html
+    html:welcome
   });
 }
 
