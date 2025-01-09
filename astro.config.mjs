589ce075e43835d0776c5b2143bd8efa6848d820
Falconiere R. Barbosa <falconieer@gmail.com>
2025-01-09 02:57:04 -0300

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
-import { rehypeHeadingIds } from '@astrojs/markdown-remark';
 import mdx from '@astrojs/mdx';
 
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
-  markdown: {
-    rehypePlugins: [
-      rehypeHeadingIds,
-    ],
-  },
 });
