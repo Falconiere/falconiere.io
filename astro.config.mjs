53f03f3a4f205b87e18e9c749c00b281481846a1
Falconiere R. Barbosa <falconieer@gmail.com>
2025-01-12 10:17:57 -0300

    testing new tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import react from '@astrojs/react';
 
 // https://astro.build/config
--- src/utils/getHeadOpenGraph.ts
+++ src/utils/getHeadOpenGraph.ts
@@ -6,24 +6,26 @@ export const getHeadOpenGraph = async (id?: string) => {
   const post = id ? await getEntry("blog", id) : undefined;
   const title = post?.data?.title ?? defaultMetaDescription.title
   const description = post?.data?.description ?? defaultMetaDescription.description;
-  const image = id ? `/api/${id}.png` : "/api/og-image.png";
-  const author = post?.data?.author ?? "Falconiere Barbosa - Blog";
-  const date = post?.data?.date ?? new Date().toISOString();
+  const image = `https://falconiere.io/${id ? `/api/${id}.png` : "/api/og-image.png"}`;
+  const author = post?.data?.author ?? defaultMetaDescription.author;
+  const date = format(new Date(post?.data?.date ?? new Date()), "yyyy-MM-dd");
   const tags = post?.data?.tags?.join(", ") ?? "";
   const url = id ? `https://falconiere.io/blog/${id}/${post?.data.createdAt}` : "https://falconiere.io";
   const site_name = "Falconiere Barbosa";
   const type = post ? "article" : "website";
   const coverAlt = post?.data?.coverAlt ?? "Falconiere Barbosa - Blog";
+  const keywords = defaultMetaDescription.keywords;
   return {
     title: id ? `${title} - Insights by Falconiere R. Barbosa` : title,
     description,
-    image: `https://falconiere.io/${image}`,
+    image,
     author,
-    date: format(new Date(date), "yyyy-MM-dd"),
+    date,
     tags,
     url,
     site_name,
     type,
-    coverAlt
+    coverAlt,
+    keywords,
   };
 };
