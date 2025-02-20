b7d00cd3b20d3d9057772ac23444958e97a6e680
Falconiere R. Barbosa <falconieer@gmail.com>
2025-02-20 14:38:43 -0300

    update scripts

--- src/data/site/defaultMetaDescription.ts
+++ src/data/site/defaultMetaDescription.ts
@@ -1,10 +1,10 @@
 export const defaultMetaDescription = {
-  title: "Falconiere R. Barbosa: Personal Reflections on Growth, Science, Technology, and Philosophy",
+  title: "Falconiere R. Barbosa",
   description: `
-Explore Falconiere.io, a personal blog by Falconiere R. Barbosa where curiosity meets reflection. Dive into insights on personal development, science, technology, philosophy, and more. Join the journey of discovery and growth!
+Explore Falconiere.io, a blog by Falconiere R. Barbosa on curiosity, growth, and insights in science, tech, philosophy, and more. Join the journey!
 `,
   summary: "A software developer by profession, a lifelong learner by passion. My interests stretch far beyond code, into the realms of science, art, and the human experience. This blog reflects my journey of growth and discovery.",
   image: "Astronaut-Headshot-Closeup.jpeg",
   author: "Falconiere R. Barbosa",
-  keywords: `Reflective blog on life, Growth and curiosity blog, Personal development blog, Science and technology blog, Philosophy and art blog, Falconiere R. Barbosa, Falconiere.io`,
+  keywords: "Reflective blog on life, Growth and curiosity blog, Personal development blog, Science and technology blog, Philosophy and art blog, Falconiere R. Barbosa, Falconiere.io",
 }
